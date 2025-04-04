835232c0dc001d88604ea355e59852b4bdc11814
Pavithra Selvaraj <71423971+Pavithra2798@users.noreply.github.com>
2025-04-04 15:46:24 +0530

    Update index.tsx

--- src/components/external-project-card/index.tsx
+++ src/components/external-project-card/index.tsx
@@ -112,8 +112,7 @@ const ExternalProjectCard = ({
                     </div>
                   )}
                   <p className="mt-2 text-base-content text-opacity-60 text-sm text-justify">
-                    {item.description}
-                    dangerouslySetInnerHTML={{ __html: item.description }}
+                     dangerouslySetInnerHTML={{ __html: item.description }}
                   </p>
                 </div>
               </div>
